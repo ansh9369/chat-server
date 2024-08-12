873d7100c602d150b864981b4dc3f0a1df51ddde
ansh9369 <152509321+ansh9369@users.noreply.github.com>
2024-08-12 17:12:19 +0530

    done

--- views/js/client.js
+++ views/js/client.js
@@ -1,5 +1,7 @@
 // const socket = io('http://localhost:8000');
-const socket = io('https://node-server-five-pearl.vercel.app/');
+const socket = io(`${window.location.protocol}//${window.location.host}`);
+
+console.log(socket);
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
